97475c7073b5c229561eaa85d3abcb944d1e55b6
Ishu Mehta <>
2022-04-17 22:47:59 +0530

    Alpha

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("home")
+})
+
+app.listen(3000, () => {
+    console.log("Port 3000 working")
+})
